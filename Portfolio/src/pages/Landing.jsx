12b40e37db933c4332e646c20524cead3e47d739
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-04-21 13:48:40 -0500

    changing link color, and adjusting landing/bio page

--- Portfolio/src/pages/Landing.jsx
+++ Portfolio/src/pages/Landing.jsx
@@ -6,15 +6,15 @@ import Footer from './partials/Footer'
 export default function Landing(){
     return(
 <>
-    <div className='rounded-xl font-mono  max-w-screen-md mx-auto' style={{backgroundColor: '#415a77', color: '#e0e1dd'}}>
+    
       <Header />
       <main className='mt-4'>
-        <div className='p-2'>
+        <div className='p-2' style={{backgroundColor: '#415a77', borderRadius: '5px'}}>
         <h3 className='text-lg font-bold'>Bio:</h3>
         <p className='text-justify'>My name is Joshua Birchfield, and I come from Huntsville, Arkansas. I grew an interest in Computer Science through a class in High School and a love for videogames. I love things that cause me to learn and challenge myself regardless of subject. One of the ways I am challenging myself is by learning Japanese. Overall, I thoroughly enjoy programming, and I hope to make intuitive programs that make life a little more convenient.</p>
         </div>
 
-        <div className='p-2 mt-4'>
+        <div className='p-2 mt-4 ' style={{backgroundColor: '#415a77', borderRadius: '5px'}}>
           <h2 className='text-xl font-bold'>Education:</h2>
           <ul className='ml-3'>
             <li>High School Diploma</li>
@@ -26,7 +26,7 @@ export default function Landing(){
       </main>
       
       <Footer />
-    </div>
+    
     
     </>
     )
